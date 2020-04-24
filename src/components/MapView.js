826e7a17b4f83ca684e6365cf4f02c5a63bd3984
Bikram Karki <himalayanepalinsis@gmail.com>
2020-04-24 17:44:24 +0300

    migrated to @ubrica/react-map-gl and mapbox-gl from leaflet

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,76 +1,92 @@
 import React, { useEffect, useState } from 'react';
-
-import { Segment, Header, Label, List } from 'semantic-ui-react';
-
-import {
-  Map,
-  TileLayer,
-  LayersControl,
-  Circle,
-  CircleMarker,
+import MapGL, {
+  Source,
+  Layer,
   Marker,
+  FeatureState,
+  NavigationControl,
+  FullscreenControl,
   Popup,
-  ZoomControl,
-  Tooltip,
-} from 'react-leaflet';
+} from '@urbica/react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-import 'leaflet/dist/leaflet.css';
+import DataService from '../services/DataServices';
+
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoiYmtybWFkdHlhIiwiYSI6ImNrOHN2bnAyNzBsdHgzc3FhYXVwczNndmcifQ.ywNlS9BxkO7FS-sYn1cMKw';
 
 const MapView = () => {
   const [data, setData] = useState([]);
+  const [hoveredCountry, setHoveredCountry] = useState();
 
-  const fetchData = async () => {
+  const _fetchData = async () => {
     const { data } = await DataService.getGlobalData();
     console.log(data[0]);
     setData(data);
   };
 
   useEffect(() => {
-    fetchData();
+    _fetchData();
   }, []);
+
+  const _onHover = (country) => {
+    setHoveredCountry(country);
+  };
+
+  const _onLeave = () => {
+    setHoveredCountry();
+  };
 
   if (!data) return null;
 
   return (
-    <>
-      <Map
-        ref={mapRef}
-        center={[17.994401411046148, -3.8671875000000004]}
-        zoom={2}
-        minZoom={2}
-        zoomControl={false}
-        doubleClickZoom={true}
-        style={{ height: '100vh' }}
-        scrollWheelZoom="true"
-      >
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-        />
+    <MapGL
+      style={{ width: '100%', height: '400px' }}
+      accessToken={MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/mapbox/light-v9"
+      latitude={37.830348}
+      longitude={-100.486052}
+      zoom={2}
+      onViewportChange={(viewport) => {}}
+      onHover={() => console.log('hovered')}
+    >
+      {data.map((country) => {
+        const { countryInfo } = country;
+        const { lat, long: lng } = countryInfo;
+        const radius = 20;
 
-        {data.map((country = {}) => {
-          const { countryInfo = {} } = country;
-          const { lat, long: lng } = countryInfo;
-          const radius = 5;
-
-          console.log(radius);
-          return (
-            <CircleMarker
-              key={country.country}
-              center={[lat, lng]}
-              color="#de3700"
-              fillOpacity={0.5}
-              radius={radius}
-              weight={0}
+        return (
+          <Marker key={country.country} longitude={lng} latitude={lat}>
+            <div
+              style={{
+                width: radius,
+                height: radius,
+                borderRadius: 50,
+                backgroundColor: 'red',
+              }}
+              onMouseOver={() => _onHover(country)}
+              onMouseLeave={() => _onLeave()}
             >
-              {/* <CountryInfo horizontalOffSet={radius + 3} info={country} /> */}
-            </CircleMarker>
-          );
-        })}
+              {hoveredCountry && hoveredCountry.country === country.country && (
+                <Popup
+                  offset={radius}
+                  longitude={lng}
+                  latitude={lat}
+                  closeButton={false}
+                  closeOnClick={false}
+                >
+                  {country.country}
+                </Popup>
+              )}
+            </div>
+          </Marker>
+        );
+      })}
 
-        <ZoomControl zoom position="bottomright" />
-      </Map>
-    </>
+      <NavigationControl showZoom position="bottom-right" />
+
+      <FullscreenControl position="top-right" />
+    </MapGL>
   );
 };
 
---
+++ src/services/DataServices.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const getGlobalData = async () => {
+  try {
+    const response = await axios.get('https://corona.lmao.ninja/v2/countries');
+    console.log(response);
+    return response;
+  } catch (e) {
+    console.log(`Fiale to fetch countries: ${e.message}`, e);
+    return;
+  }
+};
+
+const getWeeklyData = async () => {
+  try {
+    const response = await axios.get(
+      'https://corona.lmao.ninja/v2/historical/all?lastdays=30'
+    );
+    console.log(response);
+    return response;
+  } catch (e) {
+    console.log(`Failed to fetch weekly data: ${e.message}`, e);
+    return;
+  }
+};
+
+export default { getGlobalData, getWeeklyData };
