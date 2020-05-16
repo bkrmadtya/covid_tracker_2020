9d1aabdcd039c80a4618e3411d999f93c9a733e1
Bikram Karki @2020 <karki.bikram007@gmail.com>
2020-05-16 14:05:53 +0300

    updated color palette

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,9 +2,9 @@ import { blue, orange, green, grey, yellow } from '@material-ui/core/colors';
 
 export default {
   active: yellow[800],
-  cases: orange[900],
+  cases: orange[800],
   deaths: grey[500],
   recovered: green[500],
   details: blue[800],
-  infoGreen: green[400],
+  infoGreen: green[600],
 };
